2fa3605f463b8aeffde48a180a5936dce66eee02
cesar herrera <cesarherreralic@gmail.com>
2024-10-20 23:12:25 +0200

    ART-0001: adding glew and glm libs and includes

--- particlesArtOne/particlesArtOne/main.cpp
+++ particlesArtOne/particlesArtOne/main.cpp
@@ -3,5 +3,19 @@
 
 int main()
 {
+	glfwInit();
+
+	// CREATE A WINDOW BEFORE GLEW INIT!!!
+
+	// GLEW
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		glfwTerminate();
+		throw "Error::GLEW::init";
+	}
+
+	system("PAUSE"); // this will be removed soon
+
 	return 0;
 }
